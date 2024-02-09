3ec3524dc03be52c48cb73386af96e3474c7dc62
timonvanhasselt <timonvanhasselt@users.noreply.github.com>
2024-02-09 11:58:05 +0100

    Update __init__.py

--- ovos_tts_plugin_edge_tts/__init__.py
+++ ovos_tts_plugin_edge_tts/__init__.py
@@ -9,8 +9,8 @@ class EdgeTTSPlugin(TTS):
         super(EdgeTTSPlugin, self).__init__(lang, config,
                                             EdgeTTSValidator(self), 'wav')
         self.config = config.get("ovos-tts-plugin-edge-tts", {})
-        self.voice = self.config.get("voice", "nl-NL-MaartenNeural")
-        self.rate = self.config.get("rate", "+0%")  # Default to normal speed (100%)
+        self.voice = self.config.get("voice", "en-US-AriaNeural")
+        self.rate = self.config.get("rate", "+200%")  # Default to normal speed (200%); use +0% for 100% speed
         self.output_file = self.config.get("output_file", "edge_tts_output.wav")
 
     async def generate_audio(self, edge_tts_communicate, file, process):
