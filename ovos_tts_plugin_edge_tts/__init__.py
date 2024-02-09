b9ab70d80a0f0ce98feb4f700f0ae89f33e73341
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-02-09 17:42:00 +0000

    Update __init__.py

--- ovos_tts_plugin_edge_tts/__init__.py
+++ ovos_tts_plugin_edge_tts/__init__.py
@@ -7,9 +7,11 @@ class EdgeTTSPlugin(StreamingTTS):
         self.voice = self.config.get("voice", "en-US-AriaNeural")
         self.rate = self.config.get("rate", "+0%")  # use +0% for normal speed (100%)
             
-    async def stream_tts(sentence):
+    async def stream_tts(sentence, voice=None, rate=None):
         """yield chunks of TTS audio as they become available"""
-        tts = edge_tts.Communicate(sentence, self.voice, rate=self.rate)
+        voice = voice or self.voice
+        rate = rate or self.rate
+        tts = edge_tts.Communicate(sentence, voice, rate=rate)
         async for chunk in tts.stream():
             if chunk["type"] == "audio":
                 yield chunk["data"]
