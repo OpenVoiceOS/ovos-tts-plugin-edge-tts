0f4e6e5502c6c204a3d5b807765bc9b30774845b
JarbasAi <jarbasai@mailfence.com>
2024-02-10 14:00:36 +0000

    fix

--- ovos_tts_plugin_edge_tts/__init__.py
+++ ovos_tts_plugin_edge_tts/__init__.py
@@ -1,13 +1,14 @@
-from ovos_plugin_manager.templates.tts import StreamingTTS
 import edge_tts
+from ovos_plugin_manager.templates.tts import StreamingTTS
+
 
 class EdgeTTSPlugin(StreamingTTS):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.voice = self.config.get("voice", "en-US-AriaNeural")
         self.rate = self.config.get("rate", "+0%")  # use +0% for normal speed (100%)
-            
-    async def stream_tts(sentence, voice=None, rate=None):
+
+    async def stream_tts(self, sentence, voice=None, rate=None):
         """yield chunks of TTS audio as they become available"""
         voice = voice or self.voice
         rate = rate or self.rate
